e27f28e78bb62991eca217f482706e144b886c64
Justin Miller <jayman1294@gmail.com>
2019-11-13 21:32:04 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,23 @@
 class Customer
+  @all = []
+  
+  attr attr_accessor :name, :age
+  
+  def initialize(name, age)
+    @name = name
+    @age = age 
+    @@all << self
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def new_meal(waiter,total, tip = 0)
+    Meal.new(waiter,self,total, tip)
+  end
+  
+  
+  
   
 end
